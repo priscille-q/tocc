80234945943b203dd3035dda9cce85b96ea3998a
priscille quistin <arka.darell@gnail.com>
2021-10-12 06:52:45 +0100

    chore: add basic html with bootstrap

---
+++ ex4/index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="./css/bootstrap.min.css" rel="stylesheet">
+
+        <title>WWW.SUFFOLK-HOLIDAYS.CO.UK</title>
+    </head>
+    <body class="container-fluid">
+        <main>
+            <div class="row justify-content-center">
+                <div class="col">
+                    <h1>WWW.SUFFOLK-HOLIDAYS.CO.UK</h1>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-5">
+                    <form id="contact">
+                        <fieldset>
+                            <legend>Want to buy this domain ?</legend>
+                            <div class="mb-3">
+                                <label for="title" class="form-label">Title</label>
+                                <input type="text" placeholder="title" class="form-control" id="title">
+                            </div>
+                            <div class="mb-3">
+                                <label for="surname" class="form-label">Surname</label>
+                                <input type="text" placeholder="Surname" class="form-control" id="surname">
+                            </div>
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email</label>
+                                <input type="email" placeholder="Email" class="form-control" id="email">
+                            </div>
+                            <div class="mb-3">
+                                <textarea placeholder="Your message"  class="form-control" id="message"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </main>
+
+
+        <script src="./js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
+ 
