5f4bd7c1d4417e1a85acacc756932609a6666db7
priscille quistin <arka.darell@gnail.com>
2021-10-12 07:01:36 +0100

    chore: add a little bit of css

--- ex4/index.php
+++ ex4/index.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="./css/bootstrap.min.css" rel="stylesheet">
+        <link href="./css/bootstrap.min.css" rel="stylesheet" >
+        <link href="./css/app.css" rel="stylesheet" >
 
         <title>WWW.SUFFOLK-HOLIDAYS.CO.UK</title>
     </head>
